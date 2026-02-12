627ed7e7cc74c08ff58c6e5ae6f9126e4143538a
sameep-git <sameep.shah@tcu.edu>
2026-02-12 15:04:40 -0600

    containerizing frontend

--- mpc-plus/next.config.ts
+++ mpc-plus/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '9000',
+        pathname: '/**',
+      },
+      // Add your production supabase domain here too if needed
+    ],
+  },
 };
 
 export default nextConfig;
